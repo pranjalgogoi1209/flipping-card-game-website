07e2400a6fd0003f9b892b437aee2089d453cb06
Akshaykalhane <akshaykalhane1999@gmail.com>
2024-07-08 13:53:36 +0530

    added home page

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import styles from "./footer.module.css";
+import footerFrame from '/images/footer_frame.png'
 
 export default function Footer() {
-  return <div className={`flex-col-center ${styles.Footer}`}>footer</div>;
+  return <div className={`flex-col-center ${styles.Footer}`}>
+    <a href="">back to website</a>
+    <img src={footerFrame} alt="" />
+  </div>;
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import styles from "./header.module.css";
+import headerFrame from '../../assets/header/Group 1000007022.png';
+import logoImage from '../../assets/header/Colorbar-Logo-Black 1 (3).png';
 
 export default function Header() {
   return (
     <div className={`flex-col-center ${styles.Header}`}>
-      header
-      <div>
-        <img src="" alt="" />
-        <img src="" alt="" />
+      <div className={`flex-col-center ${styles.header_images}`}>
+        <img src={headerFrame} alt="" className={styles.headerFrame} />
+        <img src={logoImage} alt="" className={styles.logoImage} />
       </div>
     </div>
   );
