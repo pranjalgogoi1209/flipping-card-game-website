378bcc2ce8aa6f91b12f9371c161001a8b2011dd
pranjalgogoi1209 <gogoipranjal2022@gmail.com>
2024-07-09 13:07:12 +0530

    update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import styles from "./header.module.css";
-import headerImg from "./../../assets/header/headerImg.png";
+import headerLeftImg from "./../../assets/header/headerLeftImg.png";
+import headerRightImg from "./../../assets/header/headerRightImg.png";
 import logo from "./../../assets/header/logo.png";
 
 export default function Header() {
   return (
     <div className={`flex-col-center ${styles.Header}`}>
-      <div className={styles.headerImg}>
-        <img
-          src={headerImg}
-          alt="headerImg"
-          className={`flex-row-center ${styles.headerImg}`}
-        />
+      <div className={`flex-row-center ${styles.headerImgs}`}>
+        <div className={styles.headerLeftImg}>
+          <img src={headerLeftImg} alt="header-left-img" />
+        </div>
+        <div className={styles.headerRightImg}>
+          <img src={headerRightImg} alt="header-right-img" />
+        </div>
       </div>
       <div className={`flex-row-center ${styles.logoContainer}`}>
         <img src={logo} alt="logo" className={styles.logoImage} />
